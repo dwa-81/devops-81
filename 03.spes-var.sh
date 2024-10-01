088fe0e7dff26ee7f7037a4d7dfcf2c20fa6f7bb
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-01 21:32:42 +0530

    functions

---
+++ 03.spes-var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "all variable passed to script: $@"
+echo "number of variables passed: $#"
+echo "script name: @0"
+echo "current working directory: $PWD"
+echo "home director of current user: $HOME"
+echo " process id script executing: $$"
