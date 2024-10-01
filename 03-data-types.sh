f37bdde0941aa56b095c927eded4b4906dae0399
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-01 21:38:11 +0530

    functions

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -6,3 +6,10 @@ NUMBER2=13
 SUM=$((NUMBER1+NUMBER2))
 
 echo "total no id : $SUM"
+
+echo "all variable passed to script: $@"
+echo "number of variables passed: $#"
+echo "script name: @0"
+echo "current working directory: $PWD"
+echo "home director of current user: $HOME"
+echo " process id script executing: $$"
