96c592343d508d527884cf577c9961999826e011
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-01 21:43:59 +0530

    functions

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -9,7 +9,7 @@ echo "total no id : $SUM"
 
 echo "all variable passed to script: $@"
 echo "number of variables passed: $#"
-echo "script name: @0"
+echo "script name: $0"
 echo "current working directory: $PWD"
 echo "home director of current user: $HOME"
 echo " process id script executing: $$"
