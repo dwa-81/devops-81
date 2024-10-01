550bd3605077b60040486f39b7ed206ac0d4ffc4
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-02 01:58:52 +0530

    functions

--- 05-install.sh
+++ 05-install.sh
@@ -3,6 +3,18 @@
 USERID=$(id -u)
 
 # echo "user id is: $USERID"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then  
+   echo "$2 is...FAILED"
+   exit 1
+else 
+   echo "$2 is...SUCCESS"
+fi
+
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root"
@@ -17,6 +29,7 @@ if [ $? -ne 0 ]
 then 
    echo "git is not istalled, going to installed it.."
    dnf install git -y
+   VALIDATE $? "installing git"
 else
    echo "git is already installed, nothing to do.."
  fi     
@@ -27,6 +40,7 @@ else
  then
     echo "mysql is not installed, going to installed it.."
     dnf install mysql -y
+    VALIDATE $? "installing mysql"
 else
    echo "mysql is already installed, nothing to do it.."  
 fi     
