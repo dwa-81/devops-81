ce96e2b091134f75eae12963cb13bac90b2dc3d8
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-03 20:13:15 +0530

    shell

--- 10-redirectors.sh
+++ 10-redirectors.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
