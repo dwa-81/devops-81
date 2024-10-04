06bf94c69ee2ae525ee528a32e120e5f9123fc4d
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-05 05:06:08 +0530

    back

--- 13.backup.sh
+++ 13.backup.sh
@@ -35,7 +35,7 @@ if [ ! -z $FILES ]   # -z shows if files empty  ! expression false
 then
     echo "FILES are found"
     ZIP_FILE="$DEST_DIR/app-log-$TIMESTAMP.zip"
-     (find ${SOURCE_DIR} -name "*.log" -mtime +14) | zip "$ZIP_FILE" -@
+     find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
      if [ -f $ZIP_FILE ]
      then
         echo "successfully zipfile older than $DAYS"
