e45c8a30cbf51a6ef0e6878017b5da004fde740d
prashanth <prashanth.moodadla6645@gmail.com>
2024-10-19 05:27:57 +0530

    shell

--- 11-set.sh
+++ 11-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex  #setting the automatic exit , if we get error
+set -ex  #setting the automatic exit , if we get error, set -ex for debug
 
 echo "hello world success"
 echooo "helo world failure"
